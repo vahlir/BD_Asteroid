32fa98548de5c58f83a93375924e2059ab4efb48
vahlir <skirwan78@gmail.com>
2025-03-09 15:55:32 -0400

    L2 Game Loop complete

--- main.py
+++ main.py
@@ -12,6 +12,16 @@ pygame.init()
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True: 
+        screen.fill(color=(0, 0, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit() 
+
+                
+        pygame.display.flip()
+
     
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
