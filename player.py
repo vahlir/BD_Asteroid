eb569146e74ccb9f0e5712c788cec93679dc781f
vahlir <16930604+vahlir@users.noreply.github.com>
2025-03-10 19:27:21 -0400

    L7 Moving

--- player.py
+++ player.py
@@ -42,8 +42,8 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a]:
+        if keys[pygame.K_d]:
             # ?
             self.rotate(dt)
-        if keys[pygame.K_d]:
+        if keys[pygame.K_a]:
             self.rotate(-dt)
